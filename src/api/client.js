d52eae29a9d663e78e17a3a65833b7a36cd6a199
Raushan Yergozhiyeva <raushanm.re@gmail.com>
2021-06-30 20:43:54 +0500

    add developer variable and change it to full name for cleaning up the data

--- src/api/client.js
+++ src/api/client.js
@@ -3,23 +3,24 @@ import qs from 'qs';
 import { handleResponse, handleError } from './response';
 
 const BASE_URL = 'https://uxcandy.com/~shapoval/test-task-backend/v2';
+const developer = 'raushanmahmud';
 
 const get = (resource, params) => {
     return axios
-        .get(`${BASE_URL}${resource}?developer=Raushan`, {params})
+        .get(`${BASE_URL}${resource}?developer=${developer}`, {params})
         .then(handleResponse)
         .catch(handleError);
 };
 
 const post = (resource, data) => {
     return axios
-        .post(`${BASE_URL}${resource}?developer=Raushan`, data)
+        .post(`${BASE_URL}${resource}?developer=${developer}`, data)
         .then(handleResponse)
         .catch(handleError);
 };
 
 const encodedPost = (resource, data) => {
-    const url = `${BASE_URL}${resource}?developer=Raushan`
+    const url = `${BASE_URL}${resource}?developer=${developer}`
     return axios({
             method: 'post',
             url,
